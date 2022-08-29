c9364688900699167a43682af1466d4a8d1000e7
MariLani <zemlyanikasweet@gmail.com>
2022-08-29 22:43:58 +0300

    helloMariya

---
+++ 005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "mariya")
+{
+    Console.WriteLine("Ура, ура");
+}
+else
+{
+    Console.Write("Привет,");
+    Console.WriteLine(username);
+}
